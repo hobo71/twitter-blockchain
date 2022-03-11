95c5b81ede3af053ad168682b44d214247adfd96
sajustsmile <sajustsmile@live.com>
2022-03-11 15:02:02 +0100

    added sidebar component

---
+++ client/components/Sidebar.js
@@ -0,0 +1,29 @@
+import { FiMoreHorizontal } from 'react-icons/fi'
+import { VscTwitter } from 'react-icons/vsc'
+
+const style = {
+    wrapper: `flex-[0.7] px-8 flex flex-col`,
+    twitterIconContainer: `text-3xl m-4`,
+    tweetButton: `bg-[#1d9bf0] hover:bg-[#1b8cd8] flex items-center justify-center font-bold rounded-3xl h-[50px] mt-[20px] mt-[20px] cursor-pointer`,
+    navContainer: `flex-1`,
+    profileButton: `flex items-center mb-6 cursor-pointer hover:bg-[#333c45]`,
+    profileLeft: `flex item-center justify-center mr-4`,
+    profileImage:`height 12 w-12 rounded-full`,
+    profileRight: `flex-1 flex`,
+    details: `flex-1`,
+    name:`text-lg`,
+    handle:`text-[#8899a6]`,
+    moreContainer:`flex items-center mr-2`
+}
+function Sidebar() {
+    return(
+        <div className={style.wrapper}>
+            <div className={style.twitterIconContainer}>
+                <vscTwitter />
+            </div>
+        </div>
+    )
+
+}
+
+export default Sidebar
