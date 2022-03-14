40996aad1347a12bb1fe45833abf590de388bd2a
sajustsmile <sajustsmile@live.com>
2022-03-14 16:40:05 +0100

    added form container icons

--- client/components/Home/TweetBox.js
+++ client/components/Home/TweetBox.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useState } from 'react'
+import { useState } from 'react';
+import { BsCardImage, BsEmojiSmile } from 'react-icons/bs';
+import { RiFileGifLine, RiBarChartHorizontalFill } from 'react-icons/ri';
+import { IoMdCalendar } from 'react-icons/io'
+import { MdOutlineLocationOn } from 'react-icons/md'
+
 
 const style = {
     wrapper: `px-4 flex flex-row border-b border-[#38444d] pb-4`,
@@ -18,7 +23,10 @@ const style = {
 const TweetBox = () => {
     const [tweetMessage, setTweetMessage] = useState('')
 
-    console.log(tweetMessage)
+    const postTweet = (event) => {
+        event.preventDefault()
+        console.log(tweetMessage)
+    }
 
     return <div className={style.wrapper}>
         <div className={style.tweetBoxLeft}>
@@ -37,7 +45,14 @@ const TweetBox = () => {
                     onChange={(e) => setTweetMessage(e.target.value)}
                 />
                 <div className={style.formLowerContainer}>
-                    <div className={style.iconsContainer}></div>
+                    <div className={style.iconsContainer}>
+                        <BsCardImage className={style.icon} />
+                        <RiFileGifLine className={style.icon} />
+                        <IoMdCalendar className={style.icon} />
+                        <RiBarChartHorizontalFill className={style.icon} />
+                        <BsEmojiSmile className={style.icon} />
+                        <MdOutlineLocationOn className={style.icon} />
+                    </div>
                     <button type="submit" className={`${style.submitGeneral} ${
                         tweetMessage ? style.activeSubmit : style.inactiveSubmit
                     }`}>
