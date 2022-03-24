2e10b779dee3da8c5d98d7c970ced365ce5e7f36
sajustsmile <sajustsmile@live.com>
2022-03-25 00:16:53 +0100

    fixed auth script errors

--- client/context/TwitterContext.js
+++ client/context/TwitterContext.js
@@ -1,59 +1,76 @@
-import { createContext, useEffect, useState } from 'react';
-import { useRouter } from 'react/router';
+import { createContext, useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+//import { client } from '../lib/client'
 
 export const TwitterContext = createContext()
+
 export const TwitterProvider = ({ children }) => {
-   const [appStatus, setAppStatus] = useState('loading')
-   const [currentAccount, setCurrentAccount] = useState('')
-   const router = useRouter()
+  const [appStatus, setAppStatus] = useState('')
+  const [currentAccount, setCurrentAccount] = useState('')
+  //const [currentUser, setCurrentUser] = useState({})
+  const router = useRouter()
 
-   useEffect(()=> {
-       checkIfwalletIsConnected()
-   }, [])
-   
-    const checkIfwalletIsConnected = async () => {
-        if (!window.ethereum) return
-        try{
-            const addressArray = await window.etherium.request({
-                method: 'eth_account',
-            })
-            if (addressArray.length > 0) {
-                //connected
-                setAppStatus('connected')
-                setCurrentAccount(addressArray[0])
-            } else {
-                //not connected send to home page
-                router.push('/')
-                setAppStatus('notConnected')
-            }
-        } catch (error) {
-            console.log(error)
-        }
+  useEffect(() => {
+    checkIfWalletIsConnected()
+  }, [])
+
+
+  /* Checks if there is an active wallet connection*/
+  const checkIfWalletIsConnected = async () => {
+    if (!window.ethereum) return setAppStatus('noMetaMask')
+    try {
+      const addressArray = await window.ethereum.request({
+        method: 'eth_accounts',
+      })
+      if (addressArray.length > 0) {
+        setAppStatus('connected')
+        setCurrentAccount(addressArray[0])
+
+        createUserAccount(addressArray[0])
+      } else {
+        router.push('/')
+        setAppStatus('notConnected')
+      }
+    } catch (err) {
+      router.push('/')
+      setAppStatus('error')
     }
+  }
 
-    /*Initiates MetaMask wallet connection*/
-    const connectToWallet = async () => {
-        if(!window.ethereum) return setAppStatus('noMetaMask')
-        try {
-            setAppStatus('loading')
+  /**
+   * Initiates MetaMask wallet connection
+   */
+  const connectWallet = async () => {
+    if (!window.ethereum) return setAppStatus('noMetaMask')
+    try {
+      setAppStatus('loading')
 
-            const addressArray = await window.ethereum.request({
-                method: 'eth_requestAccount',
-            })
-            if (addressArray.length > 0) {
-                setCurrentAccount(addressArray[0])
-            }else {
-                router.push('/')
-                setAppStatus('notConnected')
-            }
-        } catch (error) {
-            setAppStatus('error')
-        }
+      const addressArray = await window.ethereum.request({
+        method: 'eth_requestAccounts',
+      })
+
+      if (addressArray.length > 0) {
+        setCurrentAccount(addressArray[0])
+        createUserAccount(addressArray[0])
+      } else {
+        router.push('/')
+        setAppStatus('notConnected')
+      }
+    } catch (err) {
+      setAppStatus('error')
     }
+  }
 
-    return(
-        <TwitterContext.Provider value={{ appStatus, currentAccount,  connectWallet }} >
-            {children}
+
+    return (
+        <TwitterContext.Provider
+          value={{
+            appStatus,
+            currentAccount,
+            connectWallet,
+          }}
+        >
+          {children}
         </TwitterContext.Provider>
-    )
-}
+      )
+    }
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,11 @@
-import Sidebar from '../components/Sidebar';
-import Feed from '../components/Home/Feed';
-import Widgets from '../components/Widgets';
+import { useContext } from 'react'
 import { TwitterContext } from '../context/TwitterContext'
-import Image from 'next/image';
-import { useContext  } from 'react';
-import MetaMaskLogo from '../assets/metamask.png';
-import ErrorIMG from '../assets/error.png';
+import Feed from '../components/home/Feed'
+import Sidebar from '../components/Sidebar'
+import Widgets from '../components/Widgets'
+import MetaMaskLogo from '../assets/metamask.png'
+import ErrorIMG from '../assets/error.png'
+import Image from 'next/image'
 
 const style = {
   wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
@@ -15,11 +15,11 @@ const style = {
   loginContent: `text-3xl font-bold text-center mt-24`,
 }
 
-export default function Home() {
-  const { appStatus, connectToWallet } = useContext(TwitterContext)
+const Home = () => {
+  const { appStatus, connectWallet } = useContext(TwitterContext)
 
   const app = (status = appStatus) => {
-    switch(status){
+    switch (status) {
       case 'connected':
         return userLoggedIn
 
@@ -37,17 +37,16 @@ export default function Home() {
     }
   }
 
-  const userLoggedIn = 
- (
-      <div className={style.content}>
-      <Sidebar />
+  const userLoggedIn = (
+    <div className={style.content}>
+      <Sidebar initialSelectedIcon={'Home'} />
       <Feed />
       <Widgets />
     </div>
   )
 
   const noUserFound = (
-  <div className={style.loginContainer}>
+    <div className={style.loginContainer}>
       <Image src={MetaMaskLogo} width={200} height={200} />
       <div
         className={style.walletConnectButton}
@@ -74,23 +73,23 @@ export default function Home() {
       </div>
     </div>
   )
-   
-  const error =(
+
+  const error = (
     <div className={style.loginContainer}>
-      <Image src={ErrorIMG} height={250} width={250} />
+      <Image src={ErrorIMG} width={250} height={200} />
       <div className={style.loginContent}>
-      An error occurred. Please try again later or from another browser.
+        An error occurred. Please try again later or from another browser.
       </div>
     </div>
   )
-const loading = (
-  <div className={style.loginContainer}>
-    <div className={style.loginContent}>loading...</div>
-  </div>
-)
-  return 
-    <div className={style.wrapper}>{app(appStatus)}</div>
-  
+
+  const loading = (
+    <div className={style.loginContainer}>
+      <div className={style.loginContent}>Loading...</div>
+    </div>
+  )
+
+  return <div className={style.wrapper}>{app(appStatus)}</div>
 }
 
-
+export default Home
