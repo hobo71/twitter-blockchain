1c24d771f018a647711d59576fc0e5e5d37f9c28
Sandra Ashipala <sajustsmile@live.com>
2022-04-22 18:52:47 +0200

    Update ProfileTweets.js

--- client/components/Profile/ProfileTweets.js
+++ client/components/Profile/ProfileTweets.js
@@ -1,4 +1,4 @@
-/* By Sandra Ashipala <https://github.com/sajustsmile> 27.03.2022*/
+/* By Sandra Ashipala <https://github.com/sandramsc> 27.03.2022*/
 import Post from '../Post';
 import { useContext } from 'react'
 import { TwitterContext } from '../../context/TwitterContext'
