a635a8105548e0a885ff954b48ac361358b17cd6
Sandra Ashipala <sajustsmile@live.com>
2022-04-22 18:53:42 +0200

    Update InitialState.js

--- client/components/mintingModal/InitialState.js
+++ client/components/mintingModal/InitialState.js
@@ -1,4 +1,4 @@
-/* By Sandra Ashipala <https://github.com/sajustsmile> 27.03.2022*/
+/* By Sandra Ashipala <https://github.com/sandramsc> 27.03.2022*/
 import { GiEarthAmerica } from 'react-icons/gi'
 
 const style = {
